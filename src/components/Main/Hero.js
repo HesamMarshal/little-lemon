ab842af38c337646346c96b945c30e08887437fd
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-06-11 13:06:26 +0330

    style the Hero section

--- src/components/Main/Hero.js
+++ src/components/Main/Hero.js
@@ -1,5 +1,20 @@
+import "./hero.css";
+import restaurantImage from "../../images/restaurant.jpg";
 const Hero = () => {
-  return <div>Hero Section</div>;
+  return (
+    <section className="hero">
+      <article>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <button>Reserve a Table</button>
+      </article>
+      <img src={restaurantImage} alt="Resturant" />
+    </section>
+  );
 };
 
 export default Hero;
