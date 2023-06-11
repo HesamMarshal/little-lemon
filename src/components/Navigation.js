50580740e339eb2ef75ac0cf61fd431c07081843
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-06-11 11:12:29 +0330

    add navigation and styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,8 +6,36 @@ const Navigation = () => {
       <img src={Logo} alt="logo" />
 
       <ul>
-        <li>Home</li>
-        <li>Home</li>
+        <li>
+          <a href="#" className="App-link">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="App-link">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" className="App-link">
+            Menu
+          </a>
+        </li>
+        <li>
+          <a href="#" className="App-link">
+            Reservation
+          </a>
+        </li>
+        <li>
+          <a href="#" className="App-link">
+            Order Online
+          </a>
+        </li>
+        <li>
+          <a href="#" className="App-link">
+            Login
+          </a>
+        </li>
       </ul>
     </nav>
   );
