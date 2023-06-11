5163db4a5c8ef7a0f5e3cbb14c9f3b62b0620bd6
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-06-11 12:26:54 +0330

    add more components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,17 @@
+import About from "./Main/About";
+import Hero from "./Main/Hero";
+import Highlights from "./Main/Highlights";
+import Testimonials from "./Main/Testimonials";
+
 const Main = () => {
-  return <main>main Section</main>;
+  return (
+    <main>
+      <Hero />
+      <Highlights />
+      <Testimonials />
+      <About />
+    </main>
+  );
 };
 
 export default Main;
