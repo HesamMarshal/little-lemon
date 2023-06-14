bc082430f60fd2fc335d0034f93add93707dba99
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-06-14 11:56:01 +0330

    add style and inputs to the form

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,5 +1,32 @@
+import Header from "../components/Header";
+import "./booking.css";
+
 const BookingPage = () => {
-  return <div>Under Construction ...</div>;
+  return (
+    <>
+      <Header></Header>
+      <form>
+        <div className="formControl">
+          <label>Date</label>
+          <input type="text" />
+        </div>
+        <div className="formControl">
+          <label>Time</label>
+          <input type="text" />
+        </div>
+        <div className="formControl">
+          <label>Number of guests</label>
+          <input type="text" />
+        </div>
+
+        <div className="formControl">
+          <label>Occasion</label>
+          <input type="text" />
+        </div>
+        <button type="submit">Reserve</button>
+      </form>
+    </>
+  );
 };
 
 export default BookingPage;
