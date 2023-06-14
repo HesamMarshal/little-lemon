393e32ce3bb91a5573eb7cbf4cc85ecd84970ad7
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-06-14 12:05:01 +0330

    use formik in the form

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,22 +1,64 @@
+import { useFormik } from "formik";
 import Header from "../components/Header";
 import "./booking.css";
 
 const BookingPage = () => {
+  // formik Values and Funtions
+  const initialValues = {
+    date: "",
+    time: "",
+    number: 1,
+    occasion: "Birth",
+  };
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  const formik = useFormik({
+    // 1. managing states
+    initialValues,
+
+    // 2. handling form submit
+    onSubmit,
+
+    // 3. validation
+    // validationSchema,
+
+    // validateOnMount: true,
+    // enableReinitialize: true,
+  });
+
   return (
     <>
       <Header></Header>
       <form>
         <div className="formControl">
-          <label>Date</label>
-          <input type="text" />
+          <label htmlFor="date">Date</label>
+          <input
+            type="text"
+            {...formik.getFieldProps("date")}
+            name="date"
+            id="date"
+          />
         </div>
         <div className="formControl">
-          <label>Time</label>
-          <input type="text" />
+          <label htmlFor="time">Time</label>
+          <input
+            type="text"
+            {...formik.getFieldProps("time")}
+            name="time"
+            id="time"
+          />
         </div>
+
         <div className="formControl">
-          <label>Number of guests</label>
-          <input type="text" />
+          <label htmlFor="number">Number of guests</label>
+          <input
+            type="text"
+            {...formik.getFieldProps("number")}
+            name="number"
+            id="number"
+          />
         </div>
 
         <div className="formControl">
