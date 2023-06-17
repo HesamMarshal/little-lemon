2539fc9477d5c6049cbe57a41f9237dfe0cf1e88
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-06-17 17:10:35 +0330

    try to do some test - not working

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,118 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+// TODO: test for
+// 1. Heading in Booking Form
+// 2. read and write in localhost
+// 3. initializeTimes & updateTimes
+
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-// import userEvent from '@testing-library/user-event';
-import Header from "./components/Header";
 
 import BookingForm from "./components/Booking/BookingForm";
-import BookingPage from "./pages/BookingPage";
+import { BrowserRouter } from "react-router-dom";
 
-// Test
-test("Check The initializeTimes Function", () => {
-  // Not Implemented
-  render(<BookingPage />);
-
-  const headingElement = screen.getByText("Book Now");
-  expect(headingElement).toBeInTheDocument();
-
-  // const results = initializeTimes();
-  // expect(results).toHaveLength(0);
+// Mount and Unmount from DOM
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
 
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+// Test App
+it("renders App", () => {
+  act(() => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      container
+    );
+  });
+
+  // const headingElement = screen.getByText("Little Lemon");
+  // expect(headingElement).toBeInTheDocument();
+  expect(container.textContent).toBe("Little Lemon");
+
+  // act(() => {
+  //   render(<Hello name="Jenny" />, container);
+  // });
+  // expect(container.textContent).toBe("Hello, Jenny!");
+
+  // act(() => {
+  //   render(<Hello name="Margaret" />, container);
+  // });
+  // expect(container.textContent).toBe("Hello, Margaret!");
+});
+
+// it("renders with or without a name", () => {
+//   act(() => {
+//     render(<BookingForm />, container);
+//   });
+//   expect(container.textContent).toBe("Hey, stranger");
+
+//   act(() => {
+//     render(<Hello name="Jenny" />, container);
+//   });
+//   expect(container.textContent).toBe("Hello, Jenny!");
+
+//   act(() => {
+//     render(<Hello name="Margaret" />, container);
+//   });
+//   expect(container.textContent).toBe("Hello, Margaret!");
+// });
+
+// import { fireEvent, render, screen } from "@testing-library/react";
+// import { useEffect, useReducer, useState } from "react";
+// import { Route, Routes, useNavigate } from "react-router-dom";
+
+// import { fetchAPI, submitAPI } from "./api/WebApi";
+
+// import { BrowserRouter } from "react-router-dom";
+// import userEvent from '@testing-library/user-event';
+// import Header from "./components/Header";
+
+// import BookingPage from "./pages/BookingPage";
+
+// Test
+// test("Check The initializeTimes Function", () => {
+//   // Not Implemented
+//   render(<BookingPage />);
+
+//   const headingElement = screen.getByText("Book Now");
+//   expect(headingElement).toBeInTheDocument();
+
+//   const results = initializeTimes();
+//   expect(results).toHaveLength(0);
+// });
+
 // test("Renders the BookingForm heading", () => {
-//   render(<BookingForm />);
+//   // const navigate = useNavigate();
+
+//   const submitForm = (formData) => {
+//     console.log("submited");
+//   };
+
+//   useEffect(() => {
+//     localStorage.setItem("bookingData", JSON.stringify(bookingData));
+//   }, [bookingData]);
+
+//   const initialState = initializeTimes();
+//   const [availableTimes, dispatch] = useReducer(updateTimes, initialState);
+
+//   render(
+//     <BookingForm
+//       availableTimes={availableTimes}
+//       dispatch={dispatch}
+//       submitForm={submitForm}
+//     />
+//   );
 //   const headingElement = screen.getByText("Book Now");
 //   expect(headingElement).toBeInTheDocument();
 // });
