cec47321e5e10cfdeea932a35111762187842c46
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-06-21 19:27:04 +0330

    change this to the

--- src/components/Main/Testimonials.jsx
+++ src/components/Main/Testimonials.jsx
@@ -15,25 +15,25 @@ const Testimonials = () => {
             star="Star 4.5"
             name="Hesam Marshal"
             imgSrc={profileImage1}
-            review="I love this pizza! It's cheesy, crispy and loaded with toppings. The sauce is tangy and the crust is soft. Best pizza ever!"
+            review="I love the pizza! It's cheesy, crispy and loaded with toppings. The sauce is tangy and the crust is soft. Best pizza ever!"
           />
           <Rating
             star="Star 4.9"
             name="Anna Sharapova"
             imgSrc={profileImage2}
-            review="This salad is so fresh and crunchy. The dressing is light and creamy, and the chicken is tender and juicy. I like the nuts and cranberries for some extra flavor and texture."
+            review="The salad is so fresh and crunchy. The dressing is light and creamy, and the chicken is tender and juicy. I like the nuts and cranberries for some extra flavor and texture."
           />
           <Rating
             star="Star 3.9"
             name="Adam Smith"
             imgSrc={profileImage3}
-            review="This cake is too dry and bland. The frosting is too sweet and artificial. I regret buying this cake. It's a waste of money and calories."
+            review="The cake is too dry and bland. The frosting is too sweet and artificial. I regret buying this cake. It's a waste of money and calories."
           />
           <Rating
             star="Star 4.3"
             name="Yuki Tsunada"
             imgSrc={profileImage4}
-            review="This soup is amazing! It's warm, hearty and comforting. The vegetables are cooked perfectly and the broth is rich and flavorful. I feel cozy and satisfied after eating this soup."
+            review="The soup is amazing! It's warm, hearty and comforting. The vegetables are cooked perfectly and the broth is rich and flavorful. I feel cozy and satisfied after eating this soup."
           />
         </div>
       </div>
