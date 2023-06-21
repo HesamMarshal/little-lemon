67bb29511165e718d2e0486785aff22c3145e5b4
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2023-06-21 17:35:11 +0330

    improve the styles and layout

--- src/components/Main/Hero.js
+++ src/components/Main/Hero.js
@@ -1,18 +1,30 @@
 import "./hero.css";
-import restaurantImage from "../../images/restaurant.jpg";
+import restaurantFood from "../../images/restaurantFood.jpg";
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
-    <section className="hero container">
-      <article>
-        <h1>Little Lemon</h1>
-        <h2>Chicago</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-        <button>Reserve a Table</button>
-      </article>
-      <img src={restaurantImage} alt="Resturant" />
+    <section id="hero">
+      <div className="hero container">
+        <article className="hero-left">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            If you're looking for a delicious and healthy meal, come to our
+            Mediterranean food restaurant. We serve a variety of dishes from
+            Greece, Turkey, Morocco, and more. You'll love our fresh salads,
+            grilled meats, savory sauces, and warm breads. Whether you want a
+            hearty gyro, a spicy tagine, or a creamy baklava, we have something
+            for everyone. Enjoy the flavors of the Mediterranean at our cozy and
+            friendly place.
+          </p>
+          <Link to="/booking">
+            <button>Reserve a Table</button>
+          </Link>
+        </article>
+        <article className="hero-right">
+          <img src={restaurantFood} alt="Resturant" className="hero-image" />
+        </article>
+      </div>
     </section>
   );
 };
